9062d9f57b07072300d8869007572c60fc6645b1
Nikita Lendiak <lendiak93@gmail.com>
2017-10-12 15:49:26 +0100

    Back button added to segue

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -22,6 +22,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
     }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 
 }
 
