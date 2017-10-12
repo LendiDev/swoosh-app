7aa105e2cde1a05941fc2cccbf84be5156a986ef
Nikita Lendiak <lendiak93@gmail.com>
2017-10-12 17:07:38 +0100

    Buttons handler added

--- Swoosh/Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override var prefersStatusBarHidden: Bool {
         return false
     }
@@ -20,7 +24,26 @@ class LeagueVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        player = Player()
+    }
+    
+    @IBAction func selectionHandler(sender: UIButton) {
+        switch sender.tag {
+        case 0: // Mens
+            selectLegue(leagueType: "mens")
+        case 1: // Womens
+            selectLegue(leagueType: "womens")
+        case 2: // Co-ed
+            selectLegue(leagueType: "coed")
+        default:
+            break
+        }
+    }
+    
+    func selectLegue(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
     
     @IBAction func onNextTapped(sender: UIButton) {
---
+++ Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Macbook on 12/10/2017.
+//  Copyright © 2017 Macbook. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
